0d108682df526d9714e4138afd439f6bf5938fb9
Lucio Niderhaus <lucholuque98@gmail.com>
2026-04-16 20:17:26 -0300

    Clase Articulo preliminar

---
+++ dominio/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public int id { get; set; }
+        public int codigo { get; set; }
+        public int nombre { get; set; }
+        public int description { get; set; }
+        public decimal precio { get; set; }
+
+    }
+}
