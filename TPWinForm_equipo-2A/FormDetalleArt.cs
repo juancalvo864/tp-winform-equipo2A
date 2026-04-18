9d55c7123d8830a60508e5c24f377be79f9e772c
Lucio Niderhaus <lucholuque98@gmail.com>
2026-04-17 21:30:04 -0300

    Re-Style FormDetalleArt

--- TPWinForm_equipo-2A/FormDetalleArt.cs
+++ TPWinForm_equipo-2A/FormDetalleArt.cs
@@ -16,5 +16,10 @@ namespace TPWinForm_equipo_2A
         {
             InitializeComponent();
         }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
