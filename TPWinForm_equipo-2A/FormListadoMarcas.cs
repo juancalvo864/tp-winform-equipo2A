77200cdfe89b2967b448170cd9d5646cab925c6c
tomasgarcia3 <tomasgarcia@integracomex.com.ar>
2026-04-17 21:14:22 -0300

    MarcaNegocio + listado de marcas en formulario

--- TPWinForm_equipo-2A/FormListadoMarcas.cs
+++ TPWinForm_equipo-2A/FormListadoMarcas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using negocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,13 @@ namespace TPWinForm_equipo_2A
         public FormListadoMarcas()
         {
             InitializeComponent();
+        }
+
+        private void FormListadoMarcas_Load(object sender, EventArgs e)
+        {
+            MarcaNegocio negocio = new MarcaNegocio();  
+            dgvMarcas.DataSource = negocio.Listar();    
+
         }
     }
 }
