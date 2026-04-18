8885155cc1940778eaf7c810a4d898c6cc9f2507
Lucio Niderhaus <lucholuque98@gmail.com>
2026-04-17 21:00:42 -0300

    Re-Size formularios

--- TPWinForm_equipo-2A/FormDetalleArt.Designer.cs
+++ TPWinForm_equipo-2A/FormDetalleArt.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormDetalleArt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormDetalleArt";
+            this.ClientSize = new System.Drawing.Size(581, 374);
+            this.ForeColor = System.Drawing.Color.Cornsilk;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormDetalleArt";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Artículo";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
