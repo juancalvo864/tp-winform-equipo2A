b36c3ecf405a2d8fd96db45c068263aea8d612a3
Lucio Niderhaus <lucholuque98@gmail.com>
2026-04-21 23:00:59 -0300

    Re-Style menues Agregar/DetalleArt

--- TPWinForm_equipo-2A/FormAgregarCat.Designer.cs
+++ TPWinForm_equipo-2A/FormAgregarCat.Designer.cs
@@ -42,10 +42,10 @@
             this.btnAgregar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAgregar.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAgregar.Location = new System.Drawing.Point(66, 162);
+            this.btnAgregar.Location = new System.Drawing.Point(11, 99);
             this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(168, 46);
+            this.btnAgregar.Size = new System.Drawing.Size(135, 27);
             this.btnAgregar.TabIndex = 48;
             this.btnAgregar.Text = "AGREGAR";
             this.btnAgregar.UseVisualStyleBackColor = false;
@@ -59,10 +59,10 @@
             this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancelar.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelar.Location = new System.Drawing.Point(396, 162);
+            this.btnCancelar.Location = new System.Drawing.Point(228, 99);
             this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(168, 46);
+            this.btnCancelar.Size = new System.Drawing.Size(135, 27);
             this.btnCancelar.TabIndex = 49;
             this.btnCancelar.Text = "CANCELAR";
             this.btnCancelar.UseVisualStyleBackColor = false;
@@ -72,27 +72,27 @@
             // 
             this.txtbCategoria.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.txtbCategoria.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.txtbCategoria.Location = new System.Drawing.Point(66, 97);
+            this.txtbCategoria.Location = new System.Drawing.Point(11, 55);
             this.txtbCategoria.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtbCategoria.Name = "txtbCategoria";
-            this.txtbCategoria.Size = new System.Drawing.Size(496, 20);
+            this.txtbCategoria.Size = new System.Drawing.Size(352, 20);
             this.txtbCategoria.TabIndex = 47;
             // 
             // lblCategoria
             // 
             this.lblCategoria.AutoSize = true;
-            this.lblCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCategoria.Location = new System.Drawing.Point(188, 43);
+            this.lblCategoria.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.Location = new System.Drawing.Point(110, 19);
             this.lblCategoria.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCategoria.Name = "lblCategoria";
-            this.lblCategoria.Size = new System.Drawing.Size(170, 17);
+            this.lblCategoria.Size = new System.Drawing.Size(154, 12);
             this.lblCategoria.TabIndex = 46;
-            this.lblCategoria.Text = "Ingresar nueva categoría:";
+            this.lblCategoria.Text = "INGRESAR NUEVA CATEGORÍA :";
             // 
             // FormAgregarCat
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(632, 269);
+            this.ClientSize = new System.Drawing.Size(378, 150);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.txtbCategoria);
